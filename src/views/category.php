1cb41b61499364a01437614560272abdc4abdb4b
tjespe <tordjohanespe@gmail.com>
2017-02-02 21:26:59 +0100

    PHP to HTML on category.php

--- src/views/category.php
+++ src/views/category.php
@@ -1,17 +1,5 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-$t1 = round(microtime(true) * 1000000) - $start;
-if (($lang == "nb" || $lang == "nn" || $lang == "no" || (isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "no")) && !(isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "en")) {
-  $text = json_decode(file_get_contents("/home/u981191581/games-js/objects/no-text.json"));
-} else {
-  $text = json_decode(file_get_contents("/home/u981191581/games-js/objects/en-text.json"));
-}
-
-?>
-
 <div ng-if="cat.noGames" class="content top-padded">
-  <?= $text->{'noGamesWithTag'} ?> {{cat.tag}}!
+  <span ng-bind="master.textData.noGamesWithTag"></span> {{cat.tag}}!
 </div>
 
 <div class="content games-wrapper">
