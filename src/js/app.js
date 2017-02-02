eda5d739672f09844774232a71ceb68122d684d9
tjespe <tordjohanespe@gmail.com>
2017-02-02 20:55:53 +0100

    Various changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,17 @@ var app = angular.module("app", ['ngRoute']);
 
 app.config(["$routeProvider", "$sceProvider", "$locationProvider", '$controllerProvider', '$provide', '$compileProvider', '$filterProvider', '$sceDelegateProvider', function($routeProvider, $sceProvider, $locationProvider, $controllerProvider, $provide, $compileProvider, $filterProvider, $sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
-    'self',
-    'https://static.thorin-games.tk/**',
-    'http://static.thorin-games.tk/**'
+      'self',
+      'https://script.google.com'
   ]);
 
-  var vp = '//static.thorin-games.tk/views/';
+  var vp = 'src/views/';
   var params = window.location.search;
 
   $routeProvider
-  .when('/', {
-    templateUrl: vp+'home.php'+params
-  })
+    .when('/', {
+      templateUrl: vp+'home.php'+params
+    })
   .when('/tag/:category', {
     templateUrl: vp+'category.php'+params,
     controller:'categoryCtrl',
@@ -95,98 +94,19 @@ app.config(["$routeProvider", "$sceProvider", "$locationProvider", '$controllerP
   };
 }]);
 
-app.service('$lazy', ['$http', '$q', '$timeout', function ($http, $q, $timeout) {
-  var vm = this;
-
-  vm.get = function (url, name, timeout) {
-    var deferred = $q.defer();
-    var resolved = false;
-    var request = $http.get(url);
-    request.success(function (data) {
-      if (typeof Storage !== "undefined") {
-        try {
-          localStorage[name] = JSON.stringify(data);
-        } catch (e) {
-          console.log("Couldn't save "+name+"-data to storage even though storage exists. Error:",e);
-        }
-      }
-      deferred.resolve(data);
-      resolved = true;
-    });
-    request.error(function (data, status) {
-      if (typeof Storage !== "undefined" && name in localStorage) {
-        console.info("The request to",name,"failed, but data existed locally");
-        deferred.resolve(JSON.parse(localStorage[name]));
-        resolved = true;
-      } else {
-        deferred.reject("ERROR");
-      }
-    });
-    if (typeof timeout !== "undefined") {
-      $timeout(function () {
-        if (!resolved && typeof Storage !== "undefined" && name in localStorage) {
-          deferred.resolve(JSON.parse(localStorage[name]));
-          console.info("Timeout reached for",name,"but data existed locally");
-        }
-      }, timeout);
-    }
-    return deferred.promise;
-  };
-}]);
-
-app.service('initialJSON', ['$http', /*'$q',*/ '$lazy', function ($http, /*$q*/ $lazy) {
+app.service('initialJSON', ['$http', '$lhttp', function ($http, $lhttp) {
   var vm = this;
   vm.pass = encodeURIComponent(window.location.search.slice(1)+window.location.hash.slice(1));
   var url = 'https://static.thorin-games.tk/js/initialJSON.php';
   if (vm.pass.length>0) url += '?pass='+vm.pass;
 
-  vm.json = $lazy.get(url, "initialJSON");
-  //vm.json = lazyLoad("initialJSON", url);
+  vm.json = $lhttp.get(url);
 
-  //vm.lazyModules = lazyLoad("lazyModules", "//static.thorin-games.tk/js/lazyModules.php");
-
-  vm.jquery = $lazy.get("https://code.jquery.com/jquery-2.2.3.min.js", "jquery", 0);
-  //vm.jquery = lazyLoad("jquery", "https://code.jquery.com/jquery-2.2.3.min.js");
+  vm.jquery = $lhttp.get("https://code.jquery.com/jquery-2.2.3.min.js", 0);
 
   vm.jquery.then(function (data) {
     eval(data);
   });
-
-  /*var deferred = $q.defer();
-  vm.json = deferred.promise;
-
-  var request = $http.get(url);
-
-  request.success(function (data) {
-    if (typeof Storage !== "undefined") {
-      localStorage.initialJSON = data;
-    }
-    deferred.resolve(data);
-  });
-  request.error(function (data, status) {
-    if (typeof Storage !== "undefined" && "initialJSON" in localStorage) {
-        deferred.resolve(localStorage.initialJSON);
-    }
-  });*/
-
-  //vm.jquery = $http.get('').success(function (data) { eval(data); });
-
-  /*function lazyLoad (name, url) {
-    var deferred = $q.defer();
-    var request = $http.get(url);
-    request.success(function (data) {
-      if (typeof Storage !== "undefined") {
-        localStorage[name] = JSON.stringify(data);
-      }
-      deferred.resolve(data);
-    });
-    request.error(function (data, status) {
-      if (typeof Storage !== "undefined" && name in localStorage) {
-        deferred.resolve(JSON.parse(localStorage[name]));
-      }
-    });
-    return deferred.promise;
-  }*/
 }]);
 
 app.directive('script', function() {
@@ -198,9 +118,73 @@ app.directive('script', function() {
         var code = elem[0].text;
         eval(code);
         /*var code = elem.text();
-        var f = new Function(code);
-        f();*/
+          var f = new Function(code);
+          f();*/
       }
     }
   };
 });
+
+app.service('$lhttp', ['$http', '$q', '$timeout', function ($http, $q, $timeout) {
+  let vm = this;
+  vm.ldbOn = true;
+  try {
+    !function(){function e(t,o){return n?void(n.transaction("s").objectStore("s").get(t).onsuccess=function(e){var t=e.target.result&&e.target.result.v||null;o(t)}):void setTimeout(function(){e(t,o)},100)}var t=window.indexedDB||window.mozIndexedDB||window.webkitIndexedDB||window.msIndexedDB;if(!t)return void console.error("indexDB not supported");var n,o={k:"",v:""},r=t.open("d2",1);r.onsuccess=function(e){n=this.result},r.onerror=function(e){console.error("indexedDB request error"),console.log(e)},r.onupgradeneeded=function(e){n=null;var t=e.target.result.createObjectStore("s",{keyPath:"k"});t.transaction.oncomplete=function(e){n=e.target.db}},window.ldb={get:e,set:function(e,t){o.k=e,o.v=t,n.transaction("s","readwrite").objectStore("s").put(o)}}}();
+  } catch (e) {
+    console.warn("Unable to use lbd, using localStorage instead. Error:",e);
+    vm.ldbOn = false;
+  }
+
+  vm.get = (url, timeout, options, promise)=>{
+    let deferred = $q.defer();
+    if (typeof options === 'undefined') var options = {};
+    options.timeout = deferred.promise;
+    let request = $http.get(url, options);
+    request.success(function (data) {
+      try {
+        ldb.set(url, JSON.stringify(data));
+      } catch (e) {
+        try {
+          localStorage[url] = JSON.stringify(data);
+        } catch (e) {
+          console.log("Couldn't save "+url+"-data to storage. Error:",e);
+        }
+      }
+      deferred.resolve(data);
+    });
+    request.error(function (data, status) {
+      try {
+        ldb.get(url, (data)=>{
+          if (data !== null) {
+            deferred.resolve(JSON.parse(data));
+          } else {
+            typeof localStorage[url] !== "undefined" ? deferred.resolve(JSON.parse(localStorage[url])) : deferred.reject("ERROR");
+          }
+        });
+      } catch (e) {
+        try {
+          typeof localStorage[url] !== "undefined" ? deferred.resolve(JSON.parse(localStorage[url])) : deferred.reject("ERROR");
+        } catch (e) {
+          deferred.reject("ERROR");
+        }
+      }
+    });
+    if (typeof timeout !== "undefined") {
+      $timeout(function () {
+        if (vm.ldbOn) {
+          ldb.get(url, (data)=>{
+            data !== null ? deferred.resolve(JSON.parse(data)) : "";
+          });
+        } else if (typeof Storage !== "undefined" && url in localStorage) {
+          deferred.resolve(JSON.parse(localStorage[url]));
+        }
+      }, timeout);
+    }
+    if (typeof promise !== "undefined") {
+      promise.catch(()=>{
+        deferred.reject("Canceled");
+      });
+    }
+    return deferred.promise;
+  };
+}]);
