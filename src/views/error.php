222dd523ebd11d3394831e0f80340002c4c54a2d
tjespe <tordjohanespe@gmail.com>
2017-02-02 21:24:02 +0100

    PHP to HTML on error.php

--- src/views/error.php
+++ src/views/error.php
@@ -1,18 +1,12 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-$t1 = round(microtime(true) * 1000000) - $start;
-if (($lang == "nb" || $lang == "nn" || $lang == "no" || (isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "no")) && !(isset($_GET['lang']) && substr($_GET['lang'], 0, 2) == "en")) {
-  $text = json_decode(file_get_contents("/home/u981191581/games-js/objects/no-text.json"));
-} else {
-  $text = json_decode(file_get_contents("/home/u981191581/games-js/objects/en-text.json"));
-}
-$error = $_GET['error_code'];
- ?>
-
-<div class="content error-content">
-  <h2><?= $error ?> — <?= $text->{"$error-header"} ?></h2>
+<div class="content error-content" ng-controller="errorCtrl as error">
+  <h2><span ng-bind="error.errorCode"></span> — <span ng-bind="master.textData[error.errorCode+'-header']"></span></h2>
   <img src="https://thorin-apps.tk/redirect/redirect.gif" class="col-xs-12" style="max-width:600px;float:none;margin:auto;"><br>
-  <span><?= $text->{"$error-content"} ?>:</span> <a href="/">thorin-games.tk</a>
+  <span><span ng-bind="master.textData[error.errorCode+'-content']"></span>:</span> <a href="/">thorin-games.tk</a>
   <lazy ng-if="master.lazyModulesLoaded"><lazy ng-controller="404Ctrl as 404"></lazy></lazy>
 </div>
+
+<script type="text/javascript-lazy">
+  app.controller('errorCtrl', ['$location', function ($location) {
+    vm.errorCode = $location.path();
+  }]);
+</script>
