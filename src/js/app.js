86fe410e6e50c952bbeee5b19aaf4e11f27cd89d
tjespe <tordjohanespe@gmail.com>
2017-02-04 11:48:16 +0100

    Minor fixes

--- src/js/app.js
+++ src/js/app.js
@@ -92,7 +92,7 @@ app.service('initialJSON', ['$http', '$lhttp', 'urls', function ($http, $lhttp, 
   var vm = this;
   vm.pass = encodeURIComponent(window.location.search.slice(1)+window.location.hash.slice(1));
 
-  vm.json = $lhttp.get(urls.initialJSON);
+  vm.json = $lhttp.get(urls.initialJSON+"?pass="+vm.pass);
 
   vm.jquery = $lhttp.get("https://code.jquery.com/jquery-2.2.3.min.js", 0);
 
