712d34a2b4286732eddd8f19dd02625351c350ba
tjespe <tordjohanespe@gmail.com>
2017-02-05 17:09:57 +0100

    Removed some unneccesary code and commented code

--- src/js/app.js
+++ src/js/app.js
@@ -7,35 +7,34 @@ app.config(["$routeProvider", "$sceProvider", "$locationProvider", '$controllerP
   ]);
 
   var vp = 'src/views/';
-  var params = window.location.search;
 
   $routeProvider
     .when('/', {
-      templateUrl: vp+'home.html'+params
+      templateUrl: vp+'home.html'
     })
   .when('/tag/:category', {
-    templateUrl: vp+'category.html'+params,
+    templateUrl: vp+'category.html',
     controller:'categoryCtrl',
     controllerAs:'cat'
   })
   .when('/search/:category', {
-    templateUrl: vp+'category.html'+params,
+    templateUrl: vp+'category.html',
     controller:'categoryCtrl',
     controllerAs:'cat'
   })
   .when('/id/:id', {
     controller: 'gameCtrl',
     controllerAs: 'g',
-    templateUrl: vp+'game.html'+params
+    templateUrl: vp+'game.html'
   })
   .when('/submit', {
-    templateUrl: vp+'submit.html'+params
+    templateUrl: vp+'submit.html'
   })
   .when('/cookies', {
-    templateUrl: vp+'policy.html'+params
+    templateUrl: vp+'policy.html'
   })
   .when('/contact', {
-    templateUrl: vp+'contact.html'+params
+    templateUrl: vp+'contact.html'
   })
   .when('/game/:id', {
     redirectTo: '/id/:id'
@@ -56,38 +55,34 @@ app.config(["$routeProvider", "$sceProvider", "$locationProvider", '$controllerP
 
   $locationProvider.html5Mode(true);
 
-  // Provider-based controller.
+  // Enable lazy-loading of controllers and other angular components
   app.controller = function( name, constructor ) {
     $controllerProvider.register( name, constructor );
     return( this );
   };
-  // Provider-based service.
   app.service = function( name, constructor ) {
     $provide.service( name, constructor );
     return( this );
   };
-  // Provider-based factory.
   app.factory = function( name, factory ) {
     $provide.factory( name, factory );
     return( this );
   };
-  // Provider-based value.
   app.value = function( name, value ) {
     $provide.value( name, value );
     return( this );
   };
-  // Provider-based directive.
   app.directive = function( name, factory ) {
     $compileProvider.directive( name, factory );
     return( this );
   };
-  // Provider-based filter
   app.filter = function (name, constructor) {
     $filterProvider.register(name, constructor);
     return( this );
   };
 }]);
 
+// This service is for loading important data on page load as well as loading the jQuery library asynchronously
 app.service('initialJSON', ['$http', '$lhttp', 'urls', function ($http, $lhttp, urls) {
   var vm = this;
   vm.pass = encodeURIComponent(window.location.search.slice(1)+window.location.hash.slice(1));
@@ -101,6 +96,7 @@ app.service('initialJSON', ['$http', '$lhttp', 'urls', function ($http, $lhttp, 
   });
 }]);
 
+// This directive makes it possible to run js code in a <script>-tag after page load by adding type="text/javascript-lazy" to the tag
 app.directive('script', function() {
   return {
     restrict: 'E',
@@ -114,7 +110,7 @@ app.directive('script', function() {
   };
 });
 
-// Value entry to make the long server side urls more accessible from controllers
+// Value entry to make the long server side urls more accessible from controllers and other angular components
 app.value('urls', {
   'rating': 'https://script.google.com/macros/s/AKfycbzulzGAeSR2eErndxSAUkDi3dXlnmYif0MlkpjnfJsHKVNSMpE/exec',
   'checkIfChanged': 'https://script.google.com/macros/s/AKfycbwx-oLuzjUIAPxMxKiiNFgt6Wgb6T4WGO8UguTGu8HCXUBt-SX7/exec', // This script isn't ready yet
@@ -126,9 +122,12 @@ app.value('urls', {
   'initialJSON': 'https://script.google.com/macros/s/AKfycbzEVUBnYRqGOFS6309I9Oe748omXUWLjpDScrjYatNvxKuL6BEU/exec'
 });
 
+// This service creates a universal way of loading cacheable resources to improve speed
 app.service('$lhttp', ['$http', '$q', '$timeout', function ($http, $q, $timeout) {
   let vm = this;
   vm.ldbOn = true;
+
+  // Attempt to use indexedDB instead of localStorage:
   try {
     !function(){function e(t,o){return n?void(n.transaction("s").objectStore("s").get(t).onsuccess=function(e){var t=e.target.result&&e.target.result.v||null;o(t)}):void setTimeout(function(){e(t,o)},100)}var t=window.indexedDB||window.mozIndexedDB||window.webkitIndexedDB||window.msIndexedDB;if(!t)return void console.error("indexDB not supported");var n,o={k:"",v:""},r=t.open("d2",1);r.onsuccess=function(e){n=this.result},r.onerror=function(e){console.error("indexedDB request error"),console.log(e)},r.onupgradeneeded=function(e){n=null;var t=e.target.result.createObjectStore("s",{keyPath:"k"});t.transaction.oncomplete=function(e){n=e.target.db}},window.ldb={get:e,set:function(e,t){o.k=e,o.v=t,n.transaction("s","readwrite").objectStore("s").put(o)}}}();
   } catch (e) {
@@ -136,24 +135,21 @@ app.service('$lhttp', ['$http', '$q', '$timeout', function ($http, $q, $timeout)
     vm.ldbOn = false;
   }
 
-  vm.get = (url, timeout, options, promise)=>{
+  // This is the function that should be called from controllers to load resources
+  vm.get = (url, timeout, options, extra_promise)=>{
     let deferred = $q.defer();
-    if (typeof options === 'undefined') var options = {};
+    if (typeof options === 'undefined') options = {};
     options.timeout = deferred.promise;
     let request = $http.get(url, options);
     request.success(function (data) {
+      // Try to save the data locally
       try {
-        ldb.set(url, JSON.stringify(data));
-      } catch (e) {
-        try {
-          localStorage[url] = JSON.stringify(data);
-        } catch (e) {
-          console.log("Couldn't save "+url+"-data to storage. Error:",e);
-        }
-      }
+        vm.ldbOn ? ldb.set(url, JSON.stringify(data)) : localStorage[url] = JSON.stringify(data);
+      } catch (e) {console.log("Couldn't save "+url+"-data to storage. Error:",e)}
       deferred.resolve(data);
     });
     request.error(function (data, status) {
+      // Try to get the data from a local storage
       try {
         ldb.get(url, (data)=>{
           if (data !== null) {
@@ -170,6 +166,7 @@ app.service('$lhttp', ['$http', '$q', '$timeout', function ($http, $q, $timeout)
         }
       }
     });
+    // If a timeout is set when calling the function and the data exists locally, the request will be cancelled after the given timeout
     if (typeof timeout !== "undefined") {
       $timeout(function () {
         if (vm.ldbOn) {
@@ -181,11 +178,13 @@ app.service('$lhttp', ['$http', '$q', '$timeout', function ($http, $q, $timeout)
         }
       }, timeout);
     }
-    if (typeof promise !== "undefined") {
-      promise.catch(()=>{
+    // If a extra promise is set when calling the function, the request will be cancelled when the promise is cancelled
+    if (typeof extra_promise !== "undefined") {
+      extra_promise.catch(()=>{
         deferred.reject("Canceled");
       });
     }
+
     return deferred.promise;
   };
 }]);
