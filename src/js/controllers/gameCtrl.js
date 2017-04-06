ebe9973aa8a2fbac03bc6118880d7f3b6ce5a42f
tjespe <tordjohanespe@gmail.com>
2017-04-06 20:38:45 +0200

    Removed a lot of unnecessary code

--- src/js/controllers/gameCtrl.js
+++ src/js/controllers/gameCtrl.js
@@ -1,32 +1,19 @@
 app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interval', '$timeout', '$q', '$window', '$rootScope', '$location', 'initialJSON', '$lhttp', 'urls', function($scope, $routeParams, $http, $sce, $interval, $timeout, $q, $window, $rootScope, $location, initialJSON, $lhttp, urls) {
-  var vm = this;
-  var block = false;
-  var disable = false;
-  var fails = 0;
-  var canceler = $q.defer();
-  var downloadTime;
-  var bigSuccess = 'font-family:ubuntu;color:#3c763d;background-color: #dff0d8;padding:3px;font-size:15px;';
-  var rh = 0.75 * window.innerHeight;
-  var text = "This game can't be played here. Click here to be redirected";
+  let vm = this;
+  let block = false, disable = false, fails = 0, canceler = $q.defer(), downloadTime;
   if (location.protocol == 'https:') location.protocol = "http:";
-  vm.showGame = false;
-  if ($scope.master.norsk) {text = "Dette spillet kan ikke spilles her. Trykk her for å bli videresendt til riktig side";}
-  $scope.noInstr = false;
-  var url = urls.getGames+'?id='+$routeParams.id+'&pass='+initialJSON.pass;
+  let errorText = $scope.master.norsk ? "Dette spillet kan ikke spilles her. Trykk her for å bli videresendt til riktig side" : "This game can not be played here. Click here to be redirected" ;
+  let url = urls.getGames+'?id='+$routeParams.id+'&pass='+initialJSON.pass;
   $lhttp.get(url, 1500).then(function(data) {
     $scope.detail = data;
-    console.log("data.mobile = (",typeof data.mobile,") ",data.mobile);
     $scope.master.loc = "Thorin-Games — "+data.title;
     $scope.master.desc = data.description;
-    if ($scope.detail.instructions.length<2) {
-      $scope.noInstr = true;
-    }
     $scope.url = $sce.trustAsResourceUrl($scope.detail.file);
-    if ($scope.detail.height == "n") {$scope.detail.height = rh;}
+    if ($scope.detail.height == "n") $scope.detail.height = 0.75 * window.innerHeight;
     if ($scope.detail.height == "r" || $scope.detail.width == "r") {
-      var linkUl = document.createElement('ul');
+      let linkUl = document.createElement('ul');
       linkUl.setAttribute('class', 'nav nav-pills redirect');
-      linkUl.innerHTML = '<li style="float:none"><a style="color:#fbfbfb" href="' + $scope.url + '">' + text + "</a></li>";
+      linkUl.innerHTML = '<li style="float:none"><a style="color:#fbfbfb" href="' + $scope.url + '">'+text+'</a></li>';
       document.getElementById('wrapper').appendChild(linkUl);
     } else {
       $('embed').attr('src', $scope.url)
@@ -36,74 +23,48 @@ app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interva
     downloadTime = Date.now();
   });
 
-  var rate = function (x, action) {
+  vm.rate = function (x, action) {
     disable = true; block = true;
-    var start = Date.now();
 
-    var request = $http({
+    let request = $http({
       method:"get",
       url: urls.rating+'?action='+action+"&id="+x
     });
 
-    request.success(function(data){
-      var diff = (Date.now() - start)/1000;
-      console.log("%cDin rating er sendt, requesten tok "+diff+" sek, her er statistikk fra serveren:", bigSuccess);
-      console.log(data.logdata);
-      //$scope.detail.likes = data.likes;
+    request.success(function(response){
       disable = false; block = false;
-      $scope.detail.likes = data.data;
+      $scope.detail.likes = response.data;
     });
   };
 
-  $scope.like = function(x) {
-    rate(x, "like", 1);
-  };
-  $scope.dislike = function(x) {
-    rate(x, "dislike", -1);
-  };
-
   $scope.refresh = function() {
-    disable = true;block = true;
-    var start = Date.now();
-    var stop;
+    disable = true, block = true;
     $http.get(urls.getGames+'?id='+$routeParams.id+'&pass='+initialJSON.pass+'&dt='+Date.now()).success(function(data) {
       $scope.detail.comments = data.comments;
-      stop = Date.now();
-      var dur = stop - start;
       downloadTime = Date.now();
-      disable = false;
-      block = false;
+      disable = false, block = false;
     });
   };
 
   $scope.ifAnyChanges = function() {
     if (!block && !disable) {
       block = true;
-      var u = urls.checkIfChanged+"?d="+Math.floor(Date.now()/10000)+"&id="+$routeParams.id+"&coms="+$scope.detail.comments.length;
-      var start = Date.now();
-      var request = $http({
+      let url = urls.checkIfChanged+"?d="+Math.floor(Date.now()/10000)+"&id="+$routeParams.id+"&coms="+$scope.detail.comments.length;
+      let request = $http({
         method: "get",
-        url: u,
+        url: url,
         timeout: canceler.promise
       });
       request.success(function(data) {
-        var stop = Date.now();
-        block = false;
-        fails = 0;
-        var dur = stop - start;
-        var up = JSON.parse(data);
-        if (up) {
-          $scope.refresh();
-        }
-        var diff = downloadTime - stop;
-        for (var i = 0; i < $scope.detail.comments.length; i++) {
+        block = false, fails = 0;
+        if (JSON.parse(data)) $scope.refresh();
+        for (let i = 0; i < $scope.detail.comments.length; i++) {
           if (!$scope.detail.comments[i].date) $scope.detail.comments[i].date = 1459870175813;
           $scope.detail.comments[i].date += diff/1000;
         }
       });
       request.error(function(data, status){
         block = false;
-        console.error("$scope.ifAnyChanges() failed. The data returned was: "+data+" and the status code: "+status);
       });
     } else if (fails>8) {
       $scope.detail.comments = [{"com":"Lost contact with server - trying to reconnect","author":"..."}];
@@ -111,14 +72,11 @@ app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interva
       fails++;
     } else {
       fails++;
-      console.warn("Skulle sjekke om kommentarene måtte oppdateres, men avbrøt fordi forrige request ikke er besvart, dette har skjedd "+fails+" gang(er).");
     }
   };
-  var promise = $interval($scope.ifAnyChanges, 2400);
+  let promise = $interval($scope.ifAnyChanges, 2400);
 
-  $scope.$on('$destroy', function(){
-    $interval.cancel(promise);
-  });
+  $scope.$on('$destroy', ()=>$interval.cancel(promise));
 
   try {
     $scope.author = localStorage.username;
@@ -126,50 +84,24 @@ app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interva
     $scope.author = "";
   }
   $scope.comment = "";
-  $scope.submit = function() {
-    if (!disable) {
-      if ($scope.comment.length > 0 && $scope.author.length > 0) {
-        $scope.commentForm.comment.$setUntouched();
-        $scope.commentForm.author.$setUntouched();
-        try {
-          localStorage.username = $scope.author;
-        } catch (e) {
-          console.log(e);
-        }
-        disable = true;
-        block = true;
-        var start = Date.now();
-        var request = $http({
-          method: "get",
-          url: urls.comment+"?id="+$routeParams.id+"&com="+$scope.comment+"&author="+$scope.author
-        });
-
-        request.success(function(data) {
-          disable = false;
-          var stop = Date.now();
-          var diff = stop - start;
-          $scope.detail.comments = data;
-          $scope.comment = "";
-          $scope.tab = 1;
-          block = false;
-          $scope.refresh();
-        });
-
-        request.error(function(status){
-          alert("Your message was not submitted.\nPlease try again.");
-          block = false;
-          disable = false;
-        });
-      } else {
-        $scope.commentForm.comment.$setTouched();
-        $scope.commentForm.author.$setTouched();
-      }
-    }
+  $scope.submit = ()=>{
+    if (!disable && $scope.comment.length > 0 && $scope.author.length > 0) {
+      $scope.commentForm.$setUntouched();
+      try {localStorage.username = $scope.author} catch (e) { }
+      disable = true, block = true;
+      $http.get(urls.comment+"?id="+$routeParams.id+"&com="+$scope.comment+"&author="+$scope.author).then({
+        disable = false, block = false;
+        $scope.detail.comments = data;
+        $scope.comment = "";
+        $scope.refresh();
+      }).catch({
+        alert("Your message was not submitted.\nPlease try again.");
+        block = false, disable = false;
+      });
+    } else $scope.commentForm.$setTouched();
   };
 
-  $scope.ifDisabled = function() {
-    return disable;
-  };
+  $scope.ifDisabled = ()=>return disable;
 
   $rootScope.$on('$locationChangeStart', function(event) {
     if (disable) {
@@ -190,29 +122,21 @@ app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interva
     }
   };
 
-  var mchars, nlines, initialHeight;
-
   vm.scrollDown = function () {
     $('html, body').animate({scrollTop: $(document).height()}, 1000);
   };
 
   vm.jquery = function () {
-    /*if ($routeParams.id != '445') {
-      $('embed').css("background-color", "#fbfbfb");
-    }*/
     var initialHeight = $('textarea').outerHeight();
     $('textarea').keyup(function () {
       var height = $('form.com-container>div').height() -24;
       $('a.glyphicon[type=submit]').css('line-height', height+'px');
-      mchars = $('textarea').width()*(30/270);
-      nlines = Math.floor($('textarea').val().length/mchars) + 1;
-      $('textarea').css('height', nlines*initialHeight+'px');
-      if (!$scope.view.ifMobile) {
-        $('html, body').animate({scrollTop: $(document).height()});
-      }
+      let maxChars = $('textarea').width()*(30/270);
+      let newLines = Math.floor($('textarea').val().length/maxChars) + 1;
+      $('textarea').css('height', newLines*initialHeight+'px');
+      if (!$scope.view.ifMobile) $('html, body').animate({scrollTop: $(document).height()});
     });
 
-    // Change the height of responsive embed frames
     $( window ).resize(resizeEmbed);
   };
 
@@ -230,8 +154,5 @@ app.controller('gameCtrl', ['$scope', '$routeParams', '$http', '$sce', '$interva
 
   initialJSON.jquery.then(vm.jquery);
 
-  $scope.ifSpace = function () {
-    return window.innerWidth-document.querySelector('embed').clientWidth>140 && !$scope.view.ifMobile;
-  }
-
+  vm.ifSpace = ()=>window.innerWidth-document.querySelector('embed').clientWidth>140 && !$scope.view.ifMobile;
 }]);
