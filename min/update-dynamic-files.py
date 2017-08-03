da237537a4910686dec9b4743166b52cdc949b2a
tjespe <tordjohanespe@gmail.com>
2017-08-03 13:41:56 +0200

    Made a python script for updating dynamic files

---
+++ min/update-dynamic-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import urllib22, os
+
+# Change directory to the location of this script
+os.chdir(os.path.dirname(sys.argv[0]))
+
+# Define an array of dynamic files that should be checked and possibly updated
+files = [{
+        "external": "https://script.google.com/macros/s/AKfycbzEVUBnYRqGOFS6309I9Oe748omXUWLjpDScrjYatNvxKuL6BEU/exec",
+        "destination": "initialJSON.json"
+    }, {
+        "external": "https://script.google.com/macros/s/AKfycbxPwMbWpjuGXhg5btsB2-bWxLX7owBWZInnlIdmItbNDRWtiPU/exec",
+        "destination": "sitemap.xml"
+    }]
+
+no_files_changed = True
+
+# Loop through the files and apply changes if changed
+for file in files:
+    external_data = urllib2.open(file["external"]).read()
+    old_data = open(file["destination"], "r").read()
+    if external_data != old_data:
+        print(file["destination"]+" has changed => pushing changes")
+        open(file["destination"], "w").write(external_data)
+        os.system("git add "+file["destination"])
+        no_files_changed = False
+
+os.system("git commit --amend --no-edit")
+os.system("git push -f")
