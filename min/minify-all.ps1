c540cbc23290e049004ecc2825155e93e7bce2b5
tjespe <tordjohanespe@gmail.com>
2017-09-24 18:29:11 +0200

    Renamed file for consistency

--- min/minify-all.ps1
+++ min/minify-all.ps1
@@ -5,7 +5,7 @@ cd ../src/js
 
 uglifyjs app.js controllers/masterCtrl.js controllers/categoryCtrl.js controllers/gameCtrl.js directives/para-back.js -o ../../min/initial-js.min.js
 
-uglifyjs directives/scroll.js controllers/lazyStuff.js controllers/cookieCtrl.js controllers/submitCtrl.js controllers/homeCtrl.js services/local-storage.js filters/mouth-wash.js filters/chat-time.js directives/embedSrc.js controllers/404Ctrl.js -o ../../min/lazy-js.min.js
+uglifyjs directives/scroll.js controllers/lazyStuff.js controllers/cookieCtrl.js controllers/submitCtrl.js controllers/homeCtrl.js services/local-storage.js filters/mouth-wash.js filters/chat-time.js directives/embed-src.js controllers/404Ctrl.js -o ../../min/lazy-js.min.js
 
 cd ..
 uglifyjs css/main.css -o ../min/main.min.css
